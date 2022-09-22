f8609e1718fa28a228d0163917060a668c21d5f5
murillo <murilloinacio16@gmail.com>
2022-09-22 08:58:36 -0300

    pequenas alterações no arquivo sistema.sh

--- sistema.sh
+++ sistema.sh
@@ -1,1 +1,13 @@
-echo "sistema teste"
+menu=0
+
+while [ $menu -ne 0 ]
+do
+	echo"Escolha o número do exercício que deseja executar. (número entre 1 e 13)"
+	read menu
+	if [ $menu -eq 1 ]
+	then
+		echo "exercicio 1"
+		else
+		echo "não escolheu o exercício 1"
+	fi
+done
