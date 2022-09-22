342d7302f4b0fbedd248e10f71a7121e5c13eb4b
murillo <murilloinacio16@gmail.com>
2022-09-22 09:42:44 -0300

    adicionando os cases, mas sem detalhes

--- sistema.sh
+++ sistema.sh
@@ -1,13 +1,24 @@
-menu=0
+menu=-1
 
 while [ $menu -ne 0 ]
 do
-	echo"Escolha o número do exercício que deseja executar. (número entre 1 e 13)"
+	echo "Escolha o número do exercício que deseja executar. (número entre 1 e 13)"
 	read menu
-	if [ $menu -eq 1 ]
-	then
-		echo "exercicio 1"
-		else
-		echo "não escolheu o exercício 1"
-	fi
+	case $menu in
+		0) echo "";;
+		1) echo "1";;
+		2) echo "2";;
+		3) echo "3";;
+		4) echo "4";;
+		5) echo "5";;
+		6) echo "6";;
+		7) echo "7";;
+		8) echo "8";;
+		9) echo "9";;
+		10) echo "10";;
+		11) echo "11";;
+		12) echo "12";;
+		13) echo "13";;
+		*) echo "ERRO";;
+	esac	
 done
