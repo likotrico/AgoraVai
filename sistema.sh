c67d6637441df942feb227b64f104c1b85832cbc
Murillo Inácio da Costa Silva <murilloinacio16@gmail.com>
2022-09-23 19:32:43 -0300

    pequena atualização nos switchs 1 e 2

--- sistema.sh
+++ sistema.sh
@@ -1,13 +1,29 @@
 menu=-1
-
+num1=0
+num2=0
 while [ $menu -ne 0 ]
 do
 	echo "Escolha o número do exercício que deseja executar. (número entre 1 e 13)"
 	read menu
 	case $menu in
 		0) echo "";;
-		1) echo "1";;
-		2) echo "2";;
+		1) echo "Elaborar um script que receba dois números como parâmetros e como resultado da sua execução exiba as seguinte informações:"
+		echo "O maior dos números recebidos."
+		echo "O somatório dos números recebidos."
+		echo "Escolha dois números:"
+		echo ""
+		echo "-Primeiro número"
+		read num1
+		echo "-Segundo número"
+		read num2
+		#bash exercicio1.sh $num1 $num2
+		;;
+	2) echo "Elaborar um script que solicite a informação de um nome de usuário e verifique se o nome informado é um usuário válido do sistema (desconsiderando as diferenças entre maiúsculas e minúsculas)."
+		echo ""
+		echo "Escolha um nome:"
+		read nome
+		#bash exercicio2.sh $nome
+		;;
 		3) echo "3";;
 		4) echo "4";;
 		5) echo "5";;
