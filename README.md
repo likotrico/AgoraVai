07bc96502e0612714a378475ae6c90d1e6190013
nasa <murilloinacio16@gmail.com>
2022-09-29 09:49:39 -0300

    Criação da pasta com os exercícios e alteração no arquivo README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# AgoraVai
+Equipe: Murillo Inácio da Costa Silva e Felipe Oliveira Rocha
+
+Modo de usar: O programa iniciará um loop, pedindo para escolher o número da questão que deseja exercutar. Visto que alguns exercícios precisam de parâmetros pra exercutar, será pedido ao usuário essas informações antes do funcionamento do programa da questão selecionada.
--- sistema.sh
+++ sistema.sh
@@ -16,33 +16,33 @@ do
 		read num1
 		echo "-Segundo número"
 		read num2
-		bash exercicio1.sh $num1 $num2
+		bash exercicios/exercicio1.sh $num1 $num2
 		;;
 	2) echo "Elaborar um script que solicite a informação de um nome de usuário e verifique se o nome informado é um usuário válido do sistema (desconsiderando as diferenças entre maiúsculas e minúsculas)."
 		echo ""
 		echo "Escolha um nome:"
 		read nome
-		bash exercicio2.sh $nome
+		bash exercicios/exercicio2.sh $nome
 		;;
 	3) echo "Escrava um script que receba como parâmetro o caminho de um diretório e liste seus arquivos e diretórios um a um. Se for um diretório, deve escrever (dir) ao lado do diretório."
 		echo ""
 		echo "Digite um diretório:"
 		read diretorio
-		bash exercicio3.sh $diretorio
+		bash exercicios/exercicio3.sh $diretorio
 		;;
 		4) clear
 			echo "Elaborar um script que receba um número como parâmetro e imprima todos os números pares de 0 até o número informado como parâmetro."
 			echo ""
 			echo "Escolha um número"
 			read num
-			bash exercicio4.sh $num
+			bash exercicios/exercicio4.sh $num
 			;;
 		5) clear
 			echo "Elaborar um script que receba um nome de arquivo como parâmetro e verifique se esse arquivo existe. Em caso afirmativo, imprimir o seu tamanho em bytes."
 			echo ""
 			echo "Digite um nome de um arquivo:"
 			read arquivo
-			bash exercicio5.sh $arquivo
+			bash exercicios/exercicio5.sh $arquivo
 			;;
 		6) echo "Elaborar um script que apresente ao usuário um menu com as seguinte opções:"
 			echo "1-Exibir status da utilização das partições do sistema; (df -h)"
@@ -51,7 +51,7 @@ do
 			echo "4-Sair"
 			echo ""
 			echo "As linhas acima serão apresentadas aos usuários até que seja escolhida a opção 4(sair). Caso seja escolhida alguma opção entre 1 e 3, serão executados os comandos correspondentes e o resultado da execução será apresentado ao usuário. Caso seja informado algum número que não corresponde às opções de menu disponíveis, exibir tela a mensagem 'Opção não disponível. Tente outra vez'."
-			bash exercicio6.sh
+			bash exercicios/exercicio6.sh
 			;;
 		7) clear
 			echo "Todos os semestres a coordenação de Sistemas de Informação exige que o professor repasse a ela os dias em que serão utilizados o laboratório 06. Essas datas baseiam-se nos dias da semana em que são ministradas as aulas. Como nossa disciplina exige o uso intensivo do laboratório, o professor repassa a coordenação todas as datas do semestre letivo em que são ministradas as aulas de TETI, para que possamos utilizar o laboratório 100% do tempo disponível. Esse é um processo trabalhoso, que envolve a busca dessas informações em um calendário e a transcrição das datas para um e-mail que é enviado à coordenação. Para minimizar esse problema, o professor quer que vocês desenvolvam um script que, a partir da informação dos dias da semana em que há aulas de TETI, produza todas as respectivas datas do ano em que serão ministradas nossas aulas."
@@ -61,23 +61,23 @@ do
 			read data2
 			echo "Escolha os dias:"
 			read dias
-			bash exercicio7.sh $data1 $data2 $dias
+			bash exercicios/exercicio7.sh $data1 $data2 $dias
 			;;
 		8) clear
 			echo "Considere um determinado diretório que possui vários arquivos textos. Esses arquivos possuem nomes variados, não obedecendo a qualquer critério. Escreva um script que renomeie cada arquivo desse diretório, dando ao arquivo o nome correspondente à primeira palavra encontrada no conteúdo do próprio arquivo."
 			echo "Digite um diretório:"
 			read diretorio
-			bash exercicio8.sh $diretorio
+			bash exercicios/exercicio8.sh $diretorio
 			;;
 		9) clear
 			echo "Desenvolva um script que receba uma lista de palavras como parâmetros e exiba, como resultado, essas palavras ordenadas."
 			echo "Digite as palavras espaçadas:"
 			read array
-			bash exercicio9.sh $array
+			bash exercicios/exercicio9.sh $array
 			;;
 		10) clear
 			echo "Desenvolva um script que receba palavras interativamente e, cada palavra recebida, exiba, como resultado, essas palavras ordenadas. Ao ser informada a palavra 'sair', o script é finalizado."
-			bash exercicio10.sh
+			bash exercicios/exercicio10.sh
 			;;
 		11) clear
 			echo "Desenvolva um script que realize as operações aritméticas básicas (soma, subtração, divisão e multiplicação), recebendo a operação e os operadores como parâmetros."
@@ -87,7 +87,7 @@ do
 			read operador
 			echo "Escolha o 2º nº: "
 			read num2
-			bash exercicio11.sh $num1 $operador $num2
+			bash exercicios/exercicio11.sh $num1 $operador $num2
 			;;
 		12) clear
 			echo "Desenvolva um script que apresente na tela os usuários cadastrados no sistema e seus respectivos diretórios home."
